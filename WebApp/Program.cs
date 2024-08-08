3186a80fc0b1a0ffe27dd222e7ecbd48cb0764b3
dsa <asd@asdasd.asdasd>
2024-08-08 21:33:01 +0300

    Temp example for dockerfile and docker-compose env parameter setting and accessing

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -7,6 +7,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+var configuration = builder.Configuration;
+
+
+// Read environment variables
+var envVar1 = configuration["ENV_VAR1"];
+var envVar2 = configuration["ENV_VAR2"];
+
+var loggingDefault = configuration["LOGGING:LOGLEVEL:DEFAULT"];
+var loggingAspentCore = configuration["LOGGING:LOGLEVEL:MICROSOFT.ASPNETCORE"];
+
+Console.WriteLine($"LOGGING__LOGLEVEL__DEFAULT: {loggingDefault}");
+Console.WriteLine($"LOGGING__LOGLEVEL__MICROSOFT__ASPNETCORE: {loggingAspentCore}");
+Console.WriteLine($"ENV_VAR1: {envVar1}");
+Console.WriteLine($"ENV_VAR2: {envVar2}");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
